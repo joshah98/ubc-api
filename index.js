9208217e83c056f86e07b3f13b2b16e79b4f1d2e
joshah98 <josha.heimann@alumni.ubc.ca>
2021-07-19 18:13:52 -0700

    finished adding/testing end points

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from "mongoose";
 import bodyParser from 'body-parser';
 import coursesRoutes from './routes/courses.js';
-import Course from './models/courseModel.js';
 import morgan from 'morgan';
 
 const app = express();
@@ -32,54 +31,3 @@ app.get('/', (req, res) => {
 })
 
 app.use('/courses', coursesRoutes);
-
-app.get('/add-course', (req, res) => {
-    const course = new Course({
-        course_code: "TEST 000",
-        course_title: "Testing db",
-        section: "000",
-        subject: "TEST",
-        summary: "Testing db summary",
-        credits: "Credits: 0",
-        prereqs: "N/A",
-        term: "Term: 0",
-        days: "N/A",
-        start: "N/A",
-        end: "N/A",
-        building: "N/A",
-        room: "N/A",
-        instructor: "N/A"
-    });
-
-    course.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-
-
-
-
-// GET /courses: return all courses
-// GET /courses/:code : courses with that code (all sections)
-// GET /courses/:code/sections : get all available sections for a specific course code
-// GET /courses/:code/:section : get just the one specific sections course info with that course code
-// GET /courses/:code/:section/start : get start time for specific course section
-// GET /courses/:code/:section/end : get end time for specific course section
-// GET /courses/:code/:section/instructor : get instructor info for a course section
-// GET /courses/:code/:section/term : get the term in which the section is being taught
-// GET /courses/:code/:section/building : get the building name in which a section is being taught
-// GET /courses/:code/:section/room : get the room number in which a course is being taught
-
-// GET /courses/:code/title: get the title of a course
-// GET /courses/:code/summary : get the summary for a course
-// GET /courses/:code/prereq : get the prerequisites for a specific course code
-// GET /courses/:code/credits : get the number of credits for a course
-
-// GET /courses/:subject : get all courses in this department
-// GET /courses/:term : get all courses in this term
-// GET /courses/:instructor : get all courses taught by this instructor
--- routes/courses.js
+++ routes/courses.js
@@ -6,6 +6,19 @@ const router = express.Router()
 // GET all courses in the session
 router.get('/', (req, res) => {
     Course.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+// GET all courses in a subject (ie. CHEM, APSC, etc.)
+router.get('/subject/:subject', (req, res) => {
+    let subj = req.params.subject;
+
+    Course.find({ subject: subj })
         .then((result) => {
             res.send(result);
         })
@@ -44,9 +57,47 @@ router.get('/:code/sections', (req, res) => {
         })
 });
 
+// GET all prerequisites for a specific course
+router.get('/:code/prereq', (req, res) => {
+    let code = req.params.code;
+    code = code.substring(0,4) + " " + code.substring(4,code.length);
+    Course.find({ course_code: code })
+     .then((result) => {
+         res.send(result[0].prereqs);
+     })
+     .catch((err) => {
+         console.log(err);
+     });
+});
+
+// GET the title of a course
+router.get('/:code/title', (req, res) => {
+    let code = req.params.code;
+    code = code.substring(0,4) + " " + code.substring(4,code.length);
+    Course.find({ course_code: code })
+     .then((result) => {
+         res.send(result[0].course_title);
+     })
+     .catch((err) => {
+         console.log(err);
+     });
+});
+
+// GET the summary of a course
+router.get('/:code/summary', (req, res) => {
+    let code = req.params.code;
+    code = code.substring(0,4) + " " + code.substring(4,code.length);
+    Course.find({ course_code: code })
+     .then((result) => {
+         res.send(result[0].summary);
+     })
+     .catch((err) => {
+         console.log(err);
+     });
+});
+
 // GET all courses with the specified course code and section
 router.get('/:code/:section', (req, res) => {
-    console.log("HERE");
     let code = req.params.code;
     let section = req.params.section;
     code = code.substring(0,4) + " " + code.substring(4,code.length);
@@ -62,20 +113,25 @@ router.get('/:code/:section', (req, res) => {
         });
 });
 
-// GET all prerequisites for a specific course
-router.get('/:code/prereq', (req, res) => {
-    let code = req.params.code;
-    console.log("HELLO");
-    code = code.substring(0,4) + " " + code.substring(4,code.length);
-    Course.find({ course_code: code })
-     .then((result) => {
-         console.log(result);
-         res.send(result);
-     })
-     .catch((err) => {
-         console.log(err);
-     });
+// POST new course into db
+router.post('/add', (req, res) => {
+    const course = new Course(req.body);
+
+    course.save()
+     .then((result) => res.send(`Course ${course.course_code} was added to the database!`))
+     .catch((err) => console.log(err));
 });
+
+// PUT update a specific courses field
+router.put('/update/:code', (req, res) => {
+    let code = req.params.code;
+    code = code.substring(0,4) + " " + code.substring(4,code.length);
+
+    Course.findOneAndUpdate({ course_code: code }, req.body)
+     .then((result) => res.send(`Successfully changed field in ${code}`))
+     .catch((err) => console.log(err));
+});
+
 
 
 
